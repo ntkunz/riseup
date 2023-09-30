446035075485812c35ff925fad531c60cf2987e1
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-09-30 16:37:09 -0700

    Add spacing between components. Add gradient color borders gallery

--- src/components/PhotoGallery/PhotoGallery.jsx
+++ src/components/PhotoGallery/PhotoGallery.jsx
@@ -11,3 +11,16 @@ export default function PhotoGallery() {
 
    )
 }
+
+
+//    <img src="/images/under-counter.JPG" alt="riseup marketplace entryway" className="gallery__image" />
+// </div>
+// <div className="gallery__frame">
+//    <img src="/images/product-wall-coffee-cocoa.JPG" alt="riseup marketplace product wall coffee and cocoa" className="gallery__image" />
+// </div>
+// <div className="gallery__frame">
+//    <img src="/images/menu-wall.JPG" alt="riseup marketplace menu board" className="gallery__image" />
+// </div>
+// <div className="gallery__frame">
+//    <img src="/images/roger-with-ting.JPG" alt="riseup marketplace owner Roger with Ting drink" className="gallery__image" />
+// </div>
