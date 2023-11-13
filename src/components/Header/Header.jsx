1631212f41167da323640613374aae7b1ce4b887
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-11-13 15:51:00 -0800

    reintroduce h1 in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,11 @@
 import "./Header.scss";
-// import MainLogo from "../../components/MainLogo/MainLogo";
-import logoOutlined from "/logos/logo-black-blur-background-medium.png";
-import logoColor from "/logos/riseup-logo-color-black-marketplace.png";
-import svgLogo from "/logos/riseup-logo-color.svg";
 import logoColorPNG from "/logos/layer-MC3.png";
 
 export default function Header() {
    return (
       <header className="header">
          <div className="header__text-container">
-            {/* <h1 className="header__title">RiseUp MarketPlace</h1> */}
+            <h1 className="header__title">RiseUp MarketPlace</h1>
             <img src={logoColorPNG} alt="RiseUp Marketplace logo laid over image of the store" className="header__logo" />
             <div className="header__details-container">
 
